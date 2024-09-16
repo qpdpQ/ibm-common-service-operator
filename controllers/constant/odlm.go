15b96570eddb80e50efdcd10bbb8121ab8b81660
Allen Li <liyuchen223@gmail.com>
2024-09-16 11:24:24 -0400

    [CD] update operandconfig for bts operator
    Signed-off-by: Allen Li <liyuchen223@gmail.com>

--- controllers/constant/odlm.go
+++ controllers/constant/odlm.go
@@ -1799,6 +1799,20 @@ spec:
           - operands:
               - name: ibm-im-operator
             registry: common-service
+  - name: ibm-bts-operator-v3.34
+    spec:
+      operandRequest:
+        requests:
+          - operands:
+              - name: ibm-im-operator
+            registry: common-service
+  - name: ibm-bts-operator-v3.35
+    spec:
+      operandRequest:
+        requests:
+          - operands:
+              - name: ibm-im-operator
+            registry: common-service
   - name: ibm-zen-operator
     spec:
       operandBindInfo: {}
